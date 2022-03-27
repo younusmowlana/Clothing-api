549a5bd98668b7fa73b410e01678cf19dff8a3e2
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-27 14:49:26 +0530

    Db_connection

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    "mongodb+srv://sharp:user1234@cluster0.qt57n.mongodb.net/shop?retryWrites=true&w=majority"
+    
+    ).then(()=>console.log("DB Connection Success"))
+    .catch((err)=>{
+        console.log(err);
+    }
+    
+    );
+    
 
 app.listen(5000, () =>{
     console.log("Backend server is running!");
