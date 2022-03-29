04a43059d58bb9a0ea377e9e65c401f8cf2ba513
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-03-29 16:00:21 +0530

    userpost-test

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ router.get("/usertest" , (req,res)=>{
 
 router.post("/userposttest" ,(req,res)=>{
     const username = req.body.username;
-    console.log(username);
+    // console.log(username);
     res.send("Your username is :" + username);
 });
 
