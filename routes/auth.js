2ae3e3654e16083fa0928650675467ea5731c0ea
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-01 01:04:08 +0530

    encrypting-with-CRYPTOJS

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,23 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //Register
+
 router.post("/register", async (req,res)=>{
     const newUser=new User ({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC)
+        .toString(),
     });
 
     try{
 
-    
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-}catch(err){
+
+    }catch(err){
     res.status(500).json(err);
 }
 
