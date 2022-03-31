80ef83d10ff2f7824808f3fb72d3c250d20fa06c
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-01 00:47:50 +0530

    Sending-data-to-the-db

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute=require("./routes/user");
+const authRoute=require("./routes/auth");
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use("/api/user", userRoute);    
+app.use("/api/auth", authRoute);    
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Backend server is running!");
