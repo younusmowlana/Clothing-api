e20db2918433dba035488c0367e29ae4429b590c
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-01 01:38:32 +0530

    Login-updated

--- routes/auth.js
+++ routes/auth.js
@@ -22,11 +22,25 @@ router.post("/register", async (req,res)=>{
 }
 });
 
-// //Login
-// router.post("/login", (req,res) => {
+//Login
+router.post("/login", async (req,res) => {
+    try{
+    const user = await User.findOne({username: req.body.username});
+        !user && res.status(401).json("Wrong credentials! ");
+    const hashedPassword = CryptoJS.AES.decrypt(
+        user.password, 
+        process.env.PASS_SEC
+        );
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+        password !==req.body.password && 
+        res.status(401).json("Wrong credentials! ");
 
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json(err);
+    }
 
-// });
+});
 
 
 module.exports = router
