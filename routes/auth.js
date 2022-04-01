1bf9bd61eba8fc2db30f6cc2ea5a3c99e56ab774
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-01 17:06:44 +0530

    fixed

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 
 //Register
-
 router.post("/register", async (req,res)=>{
     const newUser=new User ({
         username: req.body.username,
@@ -26,20 +25,26 @@ router.post("/register", async (req,res)=>{
 router.post("/login", async (req,res) => {
     try{
     const user = await User.findOne({username: req.body.username});
-        !user && res.status(401).json("Wrong credentials! ");
+    if (!user) {
+        return res.status(401).json("Wrong credentials! ");
+
+    }
+                 
     const hashedPassword = CryptoJS.AES.decrypt(
         user.password, 
         process.env.PASS_SEC
         );
         const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-        OriginalPassword !==req.body.password && 
-        res.status(401).json("Wrong credentials! ");
-
+          
+       
+if (OriginalPassword !==req.body.password) {
+   return  res.status(401).json("Wrong credentials! "); 
+}
         const { password, ...others } = user._doc;
 
         res.status(200).json(user);
     }catch(err){
-        res.status(500).json(err);
+      return  res.status(500).json(err);
     }
 
 });
