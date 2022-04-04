88ab8a6d354806c8f91aff673d7913a4bd4c1ef0
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-05 00:21:29 +0530

    JWT-genarating-access-token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //Register
 router.post("/register", async (req,res)=>{
@@ -40,9 +41,16 @@ router.post("/login", async (req,res) => {
 if (OriginalPassword !==req.body.password) {
    return  res.status(401).json("Wrong password! "); 
 }
-        const { password, ...others } = user._doc;
-
-        res.status(200).json(user);
+        const accessToken = jwt.sign({
+            id:user._id, 
+            isAdmin:user.isAdmin,
+        },
+        process.env.JWT_SEC,
+        {expiresIn:"3d"}
+        
+        );
+        const { password, ...others } = user._doc;  
+        res.status(200).json({...others, accessToken});
     }catch(err){
       return  res.status(500).json(err);
     }
