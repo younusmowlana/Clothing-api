ed222670414e183f94f092e695ce27d33bb502ad
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-05 00:37:23 +0530

    updated

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,1 +1,10 @@
 const jwt = require("jsonwebtoken");
+
+const verifyToken = (req,res,next)=>{
+    const authHeader = req.headers.token
+    if(authHeader){
+        
+    }else{
+        return res.status(401).json("You are not authenticated!");
+    }
+};
