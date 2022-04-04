66388626c9ece708f590dd0a67622e76e7a02fa0
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-05 00:48:32 +0530

    updated

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -7,8 +7,14 @@ const verifyToken = (req,res,next)=>{
             if(err){
                 return res.status(403).json("Token is not valid!");
             }
+            
+                req.user = user;
+                next();
+
+            
         });
     }else{
         return res.status(401).json("You are not authenticated!");
     }
 };
+module.exports = {verifyToken};
