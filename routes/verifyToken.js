7230c7427035ceef3e44e77cd14ab611ee695a17
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-07 00:34:22 +0530

    verify-token

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -15,17 +15,20 @@ const verifyToken = (req, res, next) => {
 };
 
 
+
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
-      next();
+      next();//you can continue ur route function
     } else {
         return  res.status(403).json("You are not alowed to do that!");
     }
   });
 };
 
-const verifyTokenAndAdmin = (req, res, next) => {
+
+
+const verifyTokenAndAdmin = (req, res, next) => { //only admmin can add any product
   verifyToken(req, res, () => {
     if (req.user.isAdmin) {
       next();
