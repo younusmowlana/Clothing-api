2047669a6048b0ea0f2c04b4561d535180bfb33a
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-07 00:34:58 +0530

    user-updating-function

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,29 @@
+const User = require("../models/User");
 const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
 
 const router = require("express").Router();
 
 //Update
-router.put("/:id" ,verifyTokenAndAuthorization,(req,res)=>{
-    
+router.put("/:id" ,verifyTokenAndAuthorization, async (req,res)=>{
+    if(req.body.password){
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password, 
+        process.env.PASS_SEC
+        ).toString();
+    }
+    try{
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+        {
+            $set:req.body,
+        },
+        {new:true}
+        
+        );
+        res.status(200).send(updatedUser);
+    }catch(err){
+       return res.status(500).json(err);
+    }
 });
 
 module.exports = router
