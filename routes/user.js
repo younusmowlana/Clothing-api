da2defa19db1d793ca215d03b05ff7dd1423c6ed
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-07 14:01:31 +0530

    sending-status

--- routes/user.js
+++ routes/user.js
@@ -31,6 +31,7 @@ router.put("/:id" ,verifyTokenAndAuthorization, async (req,res)=>{
 router.delete("/:id",verifyTokenAndAuthorization, async (req,res)=>{
 try{
     await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted!");
 }catch(err){
     return res.status(500).json(err);
 }
