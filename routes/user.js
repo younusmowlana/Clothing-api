5d3cf8fddc290e406a8b7d1ac292ffa192bbe4be
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-07 14:44:04 +0530

    get-updated

--- routes/user.js
+++ routes/user.js
@@ -28,18 +28,25 @@ router.put("/:id" ,verifyTokenAndAuthorization, async (req,res)=>{
 
 
 //Delete
-router.delete("/:id",verifyTokenAndAuthorization, async (req,res)=>{
-try{
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).json("User has been deleted!");
-}catch(err){
-    return res.status(500).json(err);
-}
-});
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
+// //Get(read)
+router.get("/find/id:",verifyTokenAndAdmin,async (req,res)=>{ //only admin get any user
+    try{
+        const user = await User.findById(req.params.id);
 
-//Get(read)
-router.get("/id:",verifyTokenAndAdmin,async (req,res)=>{ //only admin get any user
-
+        //const { password, ...others } = user._doc;  
+        res.status(200).json(user);
+    }catch(err){
+        return res.status(500).json(err);
+    }
 });
 module.exports = router
