cd41698a5637677efc65e3a3687daf24219356ad
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-10 00:55:40 +0530

    Get-all-cart

--- routes/cart.js
+++ routes/cart.js
@@ -55,15 +55,15 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-// // //GET ALL
+// //GET ALL
 
-// router.get("/", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     const carts = await Cart.find();
-//     res.status(200).json(carts);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
