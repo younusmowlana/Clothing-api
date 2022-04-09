88dd442aed797d9bf31e3299705cc555a68a90d6
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-10 00:50:43 +0530

    Create-Cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,21 @@
+const Cart = require("../models/Cart");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
 const router = require("express").Router();
 
+//CREATE
 
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
 
-module.exports = router
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
