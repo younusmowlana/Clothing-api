b591d14ed838e9b7a1b2684d64a82baad936e2e6
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-10 00:55:13 +0530

    Get-cart-by-user-id

--- routes/cart.js
+++ routes/cart.js
@@ -45,15 +45,15 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-// //GET USER CART
-// router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     const cart = await Cart.findOne({ userId: req.params.userId });
-//     res.status(200).json(cart);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//GET USER CART
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // //GET ALL
 
