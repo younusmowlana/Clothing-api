8d1438e74322ae16370f9af991cef8a52b3e6ce7
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-10 00:53:28 +0530

    Update-cart

--- routes/cart.js
+++ routes/cart.js
@@ -19,21 +19,21 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
-// //UPDATE
-// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     const updatedCart = await Cart.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body,
-//       },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedCart);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //DELETE
 // router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
