a793f66a48faae8aa4811e4c948bcd92684a1b85
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-09 14:47:31 +0530

    stats-using-Get-method

--- routes/user.js
+++ routes/user.js
@@ -68,6 +68,30 @@ router.get("/",verifyTokenAndAdmin,async (req,res)=>{ //only admin get any user 
 //Get user stats
 
 router.get("/stats",verifyTokenAndAdmin, async (req,res)=>{ //this function returns total number of users per month
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
+  try{
+
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+   
+    ]);
+    res.status(200).json(data);
+
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 module.exports = router
