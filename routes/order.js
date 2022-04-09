6b0fbb29a3e5d9f0768ad310a08e1a4035e79370
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-10 01:00:40 +0530

    Update-Order

--- routes/order.js
+++ routes/order.js
@@ -19,21 +19,21 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
-// //UPDATE
-// router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     const updatedOrder = await Order.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body,
-//       },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedOrder);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //DELETE
 // router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
