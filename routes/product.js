7bfb9a52c3c35e6e769a9fbfd12fbb5f191a974a
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-09 16:30:51 +0530

    GET ALL PRODUCTS

--- routes/product.js
+++ routes/product.js
@@ -56,29 +56,29 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
-// //GET ALL PRODUCTS
-// router.get("/", async (req, res) => {
-//   const qNew = req.query.new;
-//   const qCategory = req.query.category;
-//   try {
-//     let products;
+// //GET ALL PRODUCTS /
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
 
-//     if (qNew) {
-//       products = await Product.find().sort({ createdAt: -1 }).limit(1);
-//     } else if (qCategory) {
-//       products = await Product.find({
-//         categories: {
-//           $in: [qCategory],
-//         },
-//       });
-//     } else {
-//       products = await Product.find();
-//     }
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
 
-//     res.status(200).json(products);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
