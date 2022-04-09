3d17c565c456eebca0926340ff12d0696280b77a
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-10 00:54:27 +0530

    Delete-cart

--- routes/cart.js
+++ routes/cart.js
@@ -35,15 +35,15 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-// //DELETE
-// router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     await Cart.findByIdAndDelete(req.params.id);
-//     res.status(200).json("Cart has been deleted...");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //GET USER CART
 // router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
