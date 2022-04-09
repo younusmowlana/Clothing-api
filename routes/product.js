fde8cd89a9ecb2d57c6d674c3b2d628d1af766fb
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-09 16:19:52 +0530

    Update-product

--- routes/product.js
+++ routes/product.js
@@ -21,20 +21,20 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => { //only admin can cre
 });
 
 //UPDATE
-// router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     const updatedProduct = await Product.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body,
-//       },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedProduct);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //DELETE
 // router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
