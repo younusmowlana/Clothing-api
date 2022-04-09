34d6efd9e8e9dce4892d8cb1bb58c01d6e128ef9
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-10 00:50:31 +0530

    updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const userRoute=require("./routes/user");
 const authRoute=require("./routes/auth");
 const productRoute=require("./routes/product");
+const cartRoute=require("./routes/cart");
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/user", userRoute);    
 app.use("/api/auth", authRoute);
 app.use("/api/product",productRoute);
+app.use("/api/cart",cartRoute);
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Backend server is running!");
