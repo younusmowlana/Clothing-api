3fc983edbaf53e81a57759d4668dea6f6f94bfcb
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-11 00:03:23 +0530

    GET USER ORDERS

--- routes/order.js
+++ routes/order.js
@@ -45,15 +45,15 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// //GET USER ORDERS
-// router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     const orders = await Order.find({ userId: req.params.userId });
-//     res.status(200).json(orders);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//GET USER ORDERS
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // //GET ALL
 
