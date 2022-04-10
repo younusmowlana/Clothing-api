138b6f67b65ef59ce3c5bfd7379548518b8da5e7
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-11 00:02:59 +0530

    Delete-order

--- routes/order.js
+++ routes/order.js
@@ -35,15 +35,15 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// //DELETE
-// router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     await Order.findByIdAndDelete(req.params.id);
-//     res.status(200).json("Order has been deleted...");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //GET USER ORDERS
 // router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
