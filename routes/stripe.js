a979f7d727242385b95bcfccca16323191b8a2a3
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-04-11 15:49:17 +0530

    Stripe-Payment-method-updated

---
+++ routes/stripe.js
@@ -0,0 +1,24 @@
+
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+
+router.post("/payment", (req,res) => {
+    stripe.charges.create({
+        source:req.body.tokenId,
+        amount:req.body.amount,
+        currency:"usd",
+
+    },(stripeErr, stripeRes)=>{
+        if(stripeErr){
+          return res.status(500).json(stripeErr);
+        }
+        else{
+            return res.status(201).json(stripeRes);
+        }
+    });
+})
+
+
+
+module.exports = router
