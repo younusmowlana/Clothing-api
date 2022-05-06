bf928ffea5de9933916b44e6aeb9008aedca9b6a
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-06 09:00:20 +0530

    new route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRoute=require("./routes/auth");
 const productRoute=require("./routes/product");
 const cartRoute=require("./routes/cart");
 const orderRoute=require("./routes/order");
+const stripeRoute=require("./routes/stripe")
 const cors = require('cors');
 app.use(cors())
 
@@ -24,6 +25,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/product",productRoute);
 app.use("/api/cart",cartRoute);
 app.use("/api/order",orderRoute);
+app.use("/api/checkout",stripeRoute);
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Backend server is running!");
