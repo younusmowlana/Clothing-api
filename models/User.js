9756b1b5d3c8a8aa317a3bf7396e587f11fe92ac
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-11 21:00:29 +0530

    Updated

--- models/User.js
+++ models/User.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true, unique: true },
+    lname: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
